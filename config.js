eab661bf64dd2faa0b56e4a945f29a8be67ba606
cucho123cuc <104190853+cucho123cuc@users.noreply.github.com>
2022-04-22 14:35:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'thg hay con đĩ ngồi trc màn hình!',
+    introDesc: `tụi m là súc vật
+    súc sinh
+    bất dịch`,
+    btnIntro: 'hihi đb',
+    title: 'm chửi thề con cặc',
+    desc: 't xin 500k ',
+    btnYes: 'ok anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'mỗi ngày m phải đưa t 500k ok ko <3',
+    btnReply: 'Gửi cho Phúc <3',
+    reply: 'ok anh trai <33333333',
+    mess: 'heheboi',
+    messDesc: 'sáng nay 7h anh xin 500k nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100077278943063
 }
